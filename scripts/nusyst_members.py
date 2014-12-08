13f5d55dc2ac939be1fc2e5fbad9c8be8a36cc25
Nicholas Devenish <ndevenish@gmail.com>
2014-12-08 16:49:41 +0000

    Tidy up the systematics reader

--- scripts/nusyst_members.py
+++ scripts/nusyst_members.py
@@ -3,8 +3,11 @@
 """
 Uses clang to static-analyse NuSystematics.cxx to determine NuEvent member usage.
 
-Usage:
-  minos-nusyst-members.py [<systematics_file>]
+Usage: nusyst-members.py [--csv | --python] <systematics_file>
+
+Options:
+  --csv     Output a CSV table of the results
+  --python  Output a python data structure describing the file
 """
 
 from __future__ import absolute_import, print_function
@@ -12,14 +15,18 @@ from __future__ import absolute_import, print_function
 from collections import defaultdict
 import os, sys
 import subprocess
+import logging
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+logger = logging.getLogger()
 
 from docopt import docopt
+
 
 try:
   import clang.cindex
   from clang.cindex import CursorKind
 except ImportError as ex:
-  print ("Error: Could not import libclang; " + str(ex))
+  logger.error ("Could not import libclang; " + str(ex))
   print (__doc__)
   sys.exit(1)
 
@@ -38,7 +45,7 @@ def iterate_kind(node, kind):
 
 def find_member_accesses(start, classname):
     members = set()
-    for ref in iterate_kinds(start, [CursorKind.MEMBER_REF_EXPR]):
+    for ref in iterate_kind(start, CursorKind.MEMBER_REF_EXPR):
         
         if ref.referenced is None:
             continue
@@ -53,37 +60,6 @@ def find_member_accesses(start, classname):
             continue
         members.add(member_name)
     return members
-        #print ref.location.line, member_name, "->", owner.kind, owner.spelling
-
-def dump_tree(cursor, depths=[]):
-  SPACER_CHILD = u"\u2502 "
-  SPACER_REF   = u"\u2551 "
-
-  #print "| "*(depth-1)
-  disp = cursor.spelling or cursor.displayname or ""
-  print ("{} <line:{} col:{}> {}".format(cursor.kind.name, cursor.location.line, cursor.location.column, disp))
-  all_children = list(cursor.get_children())
-  has_reference = cursor.referenced and not cursor.referenced == cursor
-  #spacing = "".join([u"\u2502 " if x else "  " for x in depths])
-  spacing = "".join(depths)
-  for i, child in enumerate(cursor.get_children()):
-    is_last_child = (i+1 == len(all_children))
-    is_last = is_last_child and not has_reference
-    corner = u"\u2514\u2500" if is_last else u"\u251C\u2500"
-    print (spacing + corner, end='')
-    # What do we pass down as a divider?
-    if is_last:
-      depth_spacer = "  "
-    elif is_last_child:
-      depth_spacer = SPACER_REF
-    else:
-      depth_spacer = SPACER_CHILD
-    dump_tree(child, depths+[depth_spacer])
-  if cursor.referenced and not cursor.referenced == cursor:
-    print (spacing + u"\u255A\u2550", end='')
-    #dump_tree(cursor.referenced, depth, skip+1)
-    ref = cursor.referenced
-    print ("{} <line:{} col:{}> {}".format(ref.kind.name, ref.location.line, ref.location.column, ref.spelling))
 
 def find_real_class_decl(cursor, classname):
   for clsd in (x for x in iterate_kind(cursor, CursorKind.CLASS_DECL) if x.spelling == classname):
@@ -107,11 +83,10 @@ def method_has_parameter_of(method, typename):
 
 if __name__ == "__main__":
   arguments = docopt(__doc__)
-
-  arg_source_file = arguments["<systematics_file>"]
-  source_file = arg_source_file
-  if source_file and not os.path.isfile(source_file):
-    print ("Error: Could not find argument source file {}".format(source_file))
+  source_file = arguments["<systematics_file>"]
+  
+  if not os.path.isfile(source_file):
+    logger.error ("Could not find argument source file {}".format(source_file))
     sys.exit(1)
 
   # Work out the flags and/or the automatic source file
@@ -119,44 +94,39 @@ if __name__ == "__main__":
   if "SRT_PUBLIC_CONTEXT" in os.environ:
     flags += ["-I{}/include".format(os.environ["SRT_PUBLIC_CONTEXT"]),
               "-I{}/include/NtupleUtils".format(os.environ["SRT_PUBLIC_CONTEXT"])]
-    pub_source_file = os.path.join(os.environ["SRT_PUBLIC_CONTEXT"], "NtupleUtils", "NuSystematic.cxx")
-    if os.path.isfile(pub_source_file):
-      source_file = arg_source_file or pub_source_file
+  else:
+    logger.warn("No SRT_PUBLIC_CONTEXT evironment variable, may not find dependencies")
+
   if "SRT_PRIVATE_CONTEXT" in os.environ:
     # Prepend onto the list of flags
     flags = ["-I{}".format(os.environ["SRT_PRIVATE_CONTEXT"]),
               "-I{}/NtupleUtils".format(os.environ["SRT_PRIVATE_CONTEXT"])] + flags
-    priv_source_file = os.path.join(os.environ["SRT_PRIVATE_CONTEXT"], "NtupleUtils", "NuSystematic.cxx")
-    if os.path.isfile(priv_source_file):
-      source_file = arg_source_file or priv_source_file
-  # If given a source file, make sure this has flags added for it's location
-  if arg_source_file:
-    flags = ["-I{}".format(os.path.dirname(arg_source_file)),
-             "-I{}".format(os.path.normpath(os.path.join(os.path.dirname(arg_source_file), "..")))] + flags
+  else:
+    logger.warn("No SRT_PRIVATE_CONTEXT evironment variable, may not find dependencies")
 
-  if not source_file:
-    print ("Error: Could not find NuSystematic.cxx. Set context or pass in as argument.")
-    sys.exit(1)  
+  # Make sure the source file has flags added for it's location
+  flags = ["-I{}".format(os.path.dirname(source_file)),
+           "-I{}".format(os.path.normpath(os.path.join(os.path.dirname(source_file), "..")))] + flags
 
-  print ("Processing source file " + source_file, file=sys.stderr)
+  print("Processing source file " + source_file, file=sys.stderr)
   index = clang.cindex.Index.create()
   tu = index.parse(source_file, args=flags)
   node_names = set()
 
   if tu.diagnostics:
-    print ("Diagnostics:", file=sys.stderr)
-    print ("\n".join(str(x) for x in list(tu.diagnostics)), file=sys.stderr)
+    logger.warn ("Diagnostics:")
+    logger.warn ("\n".join(str(x) for x in list(tu.diagnostics)))
 
   # Find all methods of the systematics class that access NuEvent
   systematics_class = find_real_class_decl(tu.cursor, "NuSystematic")
   if not systematics_class:
-    print ("Error: Could not find class declaration for NuSystematic")
+    logger.error ("Error: Could not find class declaration for NuSystematic")
     sys.exit(1)
 
   methods = [x for x in find_class_methods(systematics_class) if method_has_parameter_of(x, "NuEvent")]
-  #dump_tree(systematics_class)
-  print ("NuSystematic methods that take NuEvent:", file=sys.stderr)
-  print (", ".join(x.spelling for x in methods), file=sys.stderr)
+  
+  logger.debug ("NuSystematic methods that take NuEvent:")
+  logger.debug (", ".join(x.spelling for x in methods))
 
   all_fields = []
   member_count = defaultdict(int)
@@ -164,10 +134,7 @@ if __name__ == "__main__":
 
   # Now, break down NuEvent accesses by method
   for method in (x.get_definition() for x in methods):
-
-    #print (method.spelling + ":")
     members = find_member_accesses(method, "NuEvent")
-    #print ("  ", members)
     member_map[method.spelling] = members
 
     for m in members:
@@ -178,11 +145,20 @@ if __name__ == "__main__":
   # Sort the member list by occurence
   all_fields = sorted(all_fields, cmp=lambda x,y: cmp(member_count[x], member_count[y]), reverse=True)
 
-  # Draw a table
-  name_len = max(len(x.spelling) for x in methods)
-  print ("Name".ljust(name_len) + ", " + ", ".join(all_fields))
-  for m in methods:
-    print (m.spelling.ljust(name_len), end=", ")
-    contains = zip(all_fields, (x in member_map[m.spelling] for x in all_fields))
-    output = [("x" if y else "").ljust(len(x)) for x, y in contains]
-    print (", ".join(output))
+  if arguments["--csv"]:
+    # Draw a table
+    name_len = max(len(x.spelling) for x in methods)
+    print ("Name".ljust(name_len) + ", " + ", ".join(all_fields))
+    for m in methods:
+      print (m.spelling.ljust(name_len), end=", ")
+      contains = zip(all_fields, (x in member_map[m.spelling] for x in all_fields))
+      output = [("x" if y else "").ljust(len(x)) for x, y in contains]
+      print (", ".join(output))
+  elif arguments["--python"]:
+    print (repr(member_map))
+  else:
+    name_len = max(len(x.spelling) for x in methods)
+    for m in [x for x in methods if member_map[x.spelling]]:
+      print (m.spelling.ljust(name_len), end=" ")
+      varlist = [x for x in all_fields if x in member_map[m.spelling]]
+      print (", ".join(varlist))
