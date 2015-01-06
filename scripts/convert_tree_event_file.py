c447ed33d94aaa1da1b37ea7e26d4cf660970073
Nicholas Devenish <ndevenish@gmail.com>
2015-01-06 16:05:52 +0000

    Change the conversion script to work in bulk

--- scripts/convert_tree_event_file.py
+++ scripts/convert_tree_event_file.py
@@ -3,7 +3,11 @@
 
 """Convert a NuFCEvent TTree to python tables
 
-Usage: convert.py <input> [<output>]"""
+Usage: convert.py [--extension=<ext>] <input> [<input>...]
+
+Options:
+  -e <ext>, --extension <ext>   The extension to append [default: hdf5]
+"""
 
 from __future__ import absolute_import, print_function
 
@@ -30,46 +34,50 @@ if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO)
   arguments = docopt(__doc__)
 
-  if not arguments["<output>"]:
-    arguments["<output>"] = os.path.splitext(arguments["<input>"])[0] + ".hdf5"
+  for inFile in arguments["<input>"]:
+    outputName = os.path.splitext(inFile)[0] + "." + arguments["--extension"].lstrip(".")
+    logger.info("Processing {}".format(inFile))
+    f = TFile(inFile)
+    if not f.IsOpen():
+      logger.error("Could not open file " + inFile)
+      sys.exit(1)
+    # Find the lone TTree
+    tree_key = {x.GetName() for x in f.GetListOfKeys() if x.GetClassName() == "TTree"}
+    if not tree_key:
+      logger.error("Could not find TTree in file " + inFile)
+      sys.exit(1)
+    if len(tree_key) > 1:
+      logger.error("More than one TTree found in file " + inFile)
+      sys.exit(1)
+    tree_name = tree_key.pop()
+    tree = f.Get(tree_name)
+    logger.info("Converting tree named " + tree_name)
 
-  f = TFile(arguments["<input>"])
-  # Find the lone TTree
-  tree_key = {x.GetName() for x in f.GetListOfKeys() if x.GetClassName() == "TTree"}
-  if not tree_key:
-    logger.error("Could not find TTree in file " + arguments["<input>"])
-    sys.exit(1)
-  if len(tree_key) > 1:
-    logger.error("More than one TTree found in file " + arguments["<input>"])
-    sys.exit(1)
-  tree_name = tree_key.pop()
-  tree = f.Get(tree_name)
-  logger.info("Converting tree named " + tree_name)
+    leaves = [x for x in tree.GetListOfLeaves() if x.GetTypeName() in ACCEPTED_TYPES and not x.GetName() in {"fBits", "fUniqueID"}]
+    # Build the numpy field list
+    dtype = [(x.GetName(), ROOT_TYPE_MAP[x.GetTypeName()]) for x in leaves]
 
-  leaves = [x for x in tree.GetListOfLeaves() if x.GetTypeName() in ACCEPTED_TYPES and not x.GetName() in {"fBits", "fUniqueID"}]
-  # Build the numpy field list
-  dtype = [(x.GetName(), ROOT_TYPE_MAP[x.GetTypeName()]) for x in leaves]
+    data = numpy.zeros(tree.GetEntries(), dtype=dtype)
 
-  data = numpy.zeros(tree.GetEntries(), dtype=dtype)
+    accessors = [TREE_ACCESS_METHOD[x.GetTypeName()](x) for x in leaves]
 
-  accessors = [TREE_ACCESS_METHOD[x.GetTypeName()](x) for x in leaves]
+    # Get the POT entry
+    pot = f.hTotalPot.Integral()
+    
+    # Loop over every entry in the tree
+    next_time = time.time() + 3
+    for entry in tree:
+      item = numpy.array(tuple(x() for x in accessors), dtype=dtype)
+      data[tree.GetReadEntry()] = item
+      if time.time() > next_time:
+        logger.info("Processing {}/{}".format(tree.GetReadEntry(), tree.GetEntries()))
+        next_time = time.time() + 3
+    logger.info("Conversion of {} items in {} POT done.".format(tree.GetEntries(), pot))
+    logger.info("Writing to output file " + outputName)
+    
 
-  # Get the POT entry
-  pot = f.hTotalPot.Integral()
-  
-  # Loop over every entry in the tree
-  next_time = time.time() + 3
-  for entry in tree:
-    item = numpy.array(tuple(x() for x in accessors), dtype=dtype)
-    data[tree.GetReadEntry()] = item
-    if time.time() > next_time:
-      logger.info("Processing {}/{}".format(tree.GetReadEntry(), tree.GetEntries()))
-      next_time = time.time() + 3
-  logger.info("Conversion of {} items in {} POT done.".format(tree.GetEntries(), pot))
-  logger.info("Writing to output file " + arguments["<output>"])
-  
-
-  outfile = tables.open_file(arguments["<output>"], mode='w', title=f.GetTitle())
-  table = outfile.create_table(outfile.root, tree.GetName(), data, tree.GetTitle())
-  table.attrs.POT = pot
-  outfile.flush()
+    outfile = tables.open_file(outputName, mode='w', title=f.GetTitle())
+    table = outfile.create_table(outfile.root, tree.GetName(), data, tree.GetTitle())
+    table.attrs.POT = pot
+    outfile.flush()
+    print ()
