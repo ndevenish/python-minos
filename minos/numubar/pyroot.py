4c1d495d7b7cd57f125175f3fc2f871b561a2dd4
Nicholas Devenish <ndevenish@gmail.com>
2015-01-19 21:48:59 +0000

    Import all the NuMuBar classes

--- minos/numubar/pyroot.py
+++ minos/numubar/pyroot.py
@@ -7,3 +7,12 @@ ROOT.gSystem.Load("libMinuit.so")
 ROOT.gSystem.Load("libBeamDataUtil.so")
 ROOT.gSystem.Load("libNtpFitSA.so")
 ROOT.gSystem.Load("libNuMuBar.so")
+
+from ROOT import (DataMCPlots, DummyModule, NuAnalysis, NuBase, NuBeam, 
+                  NuConfig, NuDSTAna, NuMadAnalysis, NuModule, NuTransSME, 
+                  NuOutputWriter, NuPIDInterface, NuPlots, NuTime, 
+                  TemplateAnalysisClass)
+__all__ = ['DataMCPlots', 'DummyModule', 'NuAnalysis', 'NuBase', 'NuBeam', 
+           'NuConfig', 'NuDSTAna', 'NuMadAnalysis', 'NuModule', 'NuTransSME', 
+           'NuOutputWriter', 'NuPIDInterface', 'NuPlots', 'NuTime', 
+           'TemplateAnalysisClass']
