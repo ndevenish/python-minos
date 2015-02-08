b6aab45645ec2af2d748bce053bd241e78f68faf
Nicholas Devenish <ndevenish@gmail.com>
2015-02-08 23:51:43 +0000

    Add LoopOverDST

---
+++ scripts/numubar/LoopOverDST.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Builds a DST summary .root file
+
+Usage:
+  LoopOverDST.py [-v] <input_file> <anaVersion> [-b SCHEME] <output_file> 
+
+Options:
+  -b SCHEME, --binning=SCHEME   The binning scheme to use [default: 4]
+  -v, --verbose                 Debug output
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+from docopt import docopt
+
+import minos.ntupleutils as ntu
+
+if __name__ == "__main__":
+  args = docopt(__doc__)
+  if args["--verbose"]:
+    logging.basicConfig(level=logging.DEBUG)
+  else:
+    logging.basicConfig(level=logging.INFO)
+
+  #print (args)
+  #assert os.path.isfile(input_file)
+  #assert os.path.isfile(xml_file)
+  
+
+  # Construct an XML file for the dst processing
+  xml = minos.ntupleutils.NuXMLConfig()
+  xml.LoadKeyValue("binningScheme", args["--binning"])
+  xml.LoadKeyValue("anaVersion", args["<anaVersion>"])
+  logger.debug("Setting up config; binning={}, ana={}".format(args["--binning"], args["<anaVersion>"]))
+
+  logger.debug("Reading {}".format(args["<input_file>"]))
+  NuBase.InputFileName(args["<input_file>"])
+  NuBase.LoadTrees(False)
+  # Create the analysis object
+  ana = NuDSTAna()
+  ana.OpenOutFile(args["<output_file>"])
+  logger.debug("Doing MMRereco")
+  ana.MMRereco("null", xml)
+  ana.WriteOutHistos()
+  ana.CloseOutFile()
+  logger.info ("End of MakeSummary")
