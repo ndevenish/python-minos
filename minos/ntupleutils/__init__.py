477c37d9e03f42b877123aa3336a29d8a71b6aea
Nicholas Devenish <ndevenish@gmail.com>
2015-03-23 22:20:36 +0000

    Default to all structs in minos.ntupleutils

--- minos/ntupleutils/__init__.py
+++ minos/ntupleutils/__init__.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 
 from .pyroot import *
-from .structs import ChargeSignTuple, OscillationParameters, NeutrinoSign, Detectorset, Detectors, HornCurrent
+from .structs import *
 from .parameters import Parameters
 from .fitter import Fitter
 from .util import (load_matrix_pair, load_spectrum_pair, binningscheme4, rebin_fd, 
                    rebin_fd_rhc, rebin_nd, binning_displayfd, PotArray, Spectrum,
-                   mapTuples)
+                   mapTuples, iterTuples)
 
-import fc
 import cuts
